341909d4bdeac7ccbb4f851ea4f1a5a18a8f39e3
surynek <pavel.surynek@seznam.cz>
2022-11-05 23:27:23 +0100

    New version of RR_xoft.

--- RR_xoft-y/src/compile.h
+++ RR_xoft-y/src/compile.h
@@ -1,7 +1,7 @@
 /*============================================================================*/
 /*                                                                            */
 /*                                                                            */
-/*                             RR_xoft 0-057_air                              */
+/*                             RR_xoft 0-058_air                              */
 /*                                                                            */
 /*                  (C) Copyright 2021 - 2022 Pavel Surynek                   */
 /*                                                                            */
@@ -9,7 +9,7 @@
 /*       http://users.fit.cvut.cz/surynek | <pavel.surynek@fit.cvut.cz>       */
 /*                                                                            */
 /*============================================================================*/
-/* compile.h / 0-057_air                                                      */
+/* compile.h / 0-058_air                                                      */
 /*----------------------------------------------------------------------------*/
 //
 // Compilation configuration file for RR_xoft package - debugging variant.
